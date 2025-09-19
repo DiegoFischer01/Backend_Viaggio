f3723274e1bf9f593830ee6f4146eaf631f7ede6
alexBeltramella <beatsinka@gmail.com>
2025-09-19 17:23:18 -0300

    creacion de USUARIO

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsuariosService } from './usuarios.service';
+import { UsuarioService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
-import { Usuario } from './usuario.entity';
+import { Usuario } from './entities/usuario.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
   controllers: [UsuariosController],
-  providers: [UsuariosService],
-  exports: [UsuariosService],
+  providers: [UsuarioService],
+  exports: [UsuarioService],
 })
 export class UsuariosModule {}
