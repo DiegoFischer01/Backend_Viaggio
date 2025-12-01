0e6d17c4829ec876646a039aa8aa758e7e1ef416
alexBeltramella <beatsinka@gmail.com>
2025-12-01 20:43:12 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.enableCors({
     origin: [
       'http://localhost:5173',
