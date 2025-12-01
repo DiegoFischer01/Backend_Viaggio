caed204765450ce47b0d2ef5d42a66bee87ea2a6
alexBeltramella <beatsinka@gmail.com>
2025-12-01 13:55:18 -0300

    arreglo de enableCors

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://backendviaggio-production.up.railway.app'
+    ],
     credentials: true,
   });
+
 
   app.useGlobalPipes(
     new ValidationPipe({
